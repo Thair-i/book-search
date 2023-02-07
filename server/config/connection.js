0400ba30020ced9d8a2335954a5dd9a556e8c4c1
Thair <110577068+Thair-i@users.noreply.github.com>
2023-02-07 09:12:26 -0800

    fix mongo db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: true,
 });
